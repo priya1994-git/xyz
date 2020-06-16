e6c3ee29d7d9fdb58e427f565e28faaf3792aee3
User <user.push@hackerrank.workspace>
2020-06-16 11:38:22 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 let maximum = () => {
-    
+  const index = require('./index')
+  const assert = require('assert')
+
+it("returns maximum of two numbers", () => {
+    assert.equal(index.maximum(1,2), 2);
+});
 }
 module.exports = {maximum}
